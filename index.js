eee98966ae74293949f01486aa39c61d1136cfe8
hlowso <h.canderleigh@gmail.com>
2018-03-13 17:00:51 +0000

    Done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if(!Array.isArray(array)) return array;
+  return array.reduce(
+    (a, b) => sumItems(a) + sumItems(b), 
+    0
+  );
 }
 
 module.exports = sumItems;
